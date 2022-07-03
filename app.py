9579a66452fce792606c1728315ae4d3861e1d3d
s1f102200570 <s1f102200570@iniad.org>
2022-07-03 14:27:15 +0900

    step2完了

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pyexpat.errors import messages
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -7,14 +8,21 @@ messages=['Hi1','Hi2']
 
 @app.route('/')
 def top():
+    kinoko_percent=kinoko_count/(kinoko_count+takenoko_count)*100
+    takenoko_percent=takenoko_count/(kinoko_count+takenoko_count)*100
     return render_template('index.html', **vars())
 @app.route('/vote', methods=['POST'])
 def answer():
+    # 投票による更新
+    global kinoko_count, takenoko_count, messages
+    if request.form.get('item')=='kinoko':
+        kinoko_count += 1
+    elif request.form.get('item')=='takenoko':
+        takenoko_count += 1
+
     # 進捗グラフの更新
     kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
     takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
-
     return render_template('vote.html', **vars())
-
 if __name__ == '__main__':
     app.run(debug=True)
